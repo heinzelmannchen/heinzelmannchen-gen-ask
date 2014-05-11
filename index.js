053c8f1847ab83e5281af0f792bb34c60633b2b9
Christoph Hermann <schtoeffel@gmail.com>
2014-05-11 15:59:40 +0200

    adds functionality to pass inquirer definition objects to the generator

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 var Q = require('q'),
     Parent = require('heinzelmannchen-generator'),
+    inquirer = require('inquirer'),
     Generator = Parent.inherit();
 
 Generator.prototype.createData = function() {
+    var q = Q.defer();
+    inquirer.prompt(this.config.ask, function(answers){
+        q.resolve(answers);
+    });
+    return q.promise;
 };
 
 Generator.explain = function() {
--- integrationTest/test.js
+++ integrationTest/test.js
@@ -1,9 +1,31 @@
 var G = require('../index'),
     g = new G();
 g.setConfig({
+    ask: [{
+        type: 'input',
+        name: 'appName',
+        message: 'Enter a name for your app'
+    }, {
+        type: 'checkbox',
+        message: 'Select toppings',
+        name: 'toppings',
+        choices: [{
+            name: 'Peperonni'
+        }, {
+            name: 'Cheese',
+            checked: true
+        }, {
+            name: 'Mushroom'
+        }, {
+            name: 'Bacon'
+        }, {
+            name: 'Olives',
+            disabled: 'out of stock'
+        }, {
+            name: 'Extra cheese'
+        }]
+    }]
 });
 g.createData().then(function(data) {
     console.dir(data);
 });
-
-console.log(g.explain());
